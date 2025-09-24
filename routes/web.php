d9b18d26fc3aa59753e1c117278f5345435fa505
farid <farid24si@pcr.ac.id>
2025-09-24 09:27:22 +0700

    home sudah selesai

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,6 +11,7 @@ Route::get('/pcr', function () {
     return 'Selamat Datang di Website Kampus PCR!';
 });
 Route::get('/mahasiswa/{param1}',[MahasiswaController::class,'show'] )->name('mahasiswa.show');
+Route::get('/home',[HomeController::class,'index']);
 Route::get('/nama/{param1}/{Nim}', function ($param1='',$Nim='') {
     return 'Nama saya: '.$param1. '<br>Nim: '.$Nim;
 });
