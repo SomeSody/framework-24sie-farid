521530514796422fb3ff2a5e8b37b23c15a0dfac
SomeSody <hanturumzh123@gmail.com>
2025-10-22 03:59:56 +0700

    percobaan sendiri

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PelangganController;
+use App\Http\Controllers\JenisSuratController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -31,3 +32,5 @@ Route::post('question/store', [QuestionController::class, 'store'])
 Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::resource('pelanggan', PelangganController::class);
+
+Route::resource('jenis_surat', JenisSuratController::class);
